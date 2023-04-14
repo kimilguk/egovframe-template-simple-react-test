b55c2ea580dd06b356f9fb1d2f2ce6dbf1e965ce
kimilguk <boramcom@daum.net>
2023-04-14 20:21:40 +0900

    사이트관리자 암호변경 기능 추가

--- src/components/EgovHeader.jsx
+++ src/components/EgovHeader.jsx
@@ -143,6 +143,7 @@ function EgovHeader({ loginUser, onChangeLogin }) {
                                 <li><NavLink to={URL.ADMIN_USAGE} className={({ isActive }) => (isActive ? "cur" : "")}>게시판사용관리</NavLink></li>
                                 <li><NavLink to={URL.ADMIN_NOTICE} className={({ isActive }) => (isActive ? "cur" : "")}>공지사항관리</NavLink></li>
                                 <li><NavLink to={URL.ADMIN_GALLERY} className={({ isActive }) => (isActive ? "cur" : "")}>사이트갤러리관리</NavLink></li>
+								<li><NavLink to={URL.ADMIN_MANAGER} className={({ isActive }) => (isActive ? "cur" : "")}>사이트관리자 암호변경</NavLink></li>
                             </ul>
                         </div>
                     }
@@ -209,6 +210,7 @@ function EgovHeader({ loginUser, onChangeLogin }) {
                                     <li><NavLink to={URL.ADMIN_USAGE} className={({ isActive }) => (isActive ? "cur" : "")}>게시판사용관리</NavLink></li>
                                     <li><NavLink to={URL.ADMIN_NOTICE} className={({ isActive }) => (isActive ? "cur" : "")}>공지사항관리</NavLink></li>
                                     <li><NavLink to={URL.ADMIN_GALLERY} className={({ isActive }) => (isActive ? "cur" : "")}>사이트갤러리관리</NavLink></li>
+									<li><NavLink to={URL.ADMIN_MANAGER} className={({ isActive }) => (isActive ? "cur" : "")}>사이트관리자 암호변경</NavLink></li>
                                 </ul>
                             </div>
                         </>
