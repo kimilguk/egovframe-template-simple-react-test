50fc362cb7e8dd7c6ff0b016babd1c3355e1b733
kimilguk <boramcom@daum.net>
2023-04-15 13:42:53 +0900

    사이트관리자 암호변경 백엔드 URL변경

--- src/pages/admin/manager/EgovAdminPasswordUpdate.jsx
+++ src/pages/admin/manager/EgovAdminPasswordUpdate.jsx
@@ -26,7 +26,7 @@ function EgovAdminPasswordUpdate(props) {
     };
 
     const updateAdminPassword = () => {
-		const editURL = "/uat/esm/updateAdminPassword.do";
+		const editURL = "/uat/esm/updateAdminPasswordAPI.do";
         const jToken = localStorage.getItem('jToken');
         let requestOptions ={};
         const formData = new FormData();
